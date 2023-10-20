68be93b5f98c06f21da4abb8ce3462405450f42f
Docker <docker@example.com>
2023-10-20 21:38:19 +0100

    feat: update BBCStepTester

--- execs/run_test.ml
+++ execs/run_test.ml
@@ -38,18 +38,18 @@ let () =
   
   let bbc_tests =
     let name : string = "compare" in
+    let runtime : runtime = compileout in
     let oracle : oracle = Expected in
-    let runtime : runtime = Runtime compileout in
-    let action : action = Compare in
-    tests_from_dir ~name ~compiler ~oracle ~runtime ~ action "bbctests" in
+    let action : action = CompareOutput in
+    tests_from_dir ~name ~compiler ~runtime ~oracle ~ action "bbctests" in
   
   let verify_tests =
-    let command =
+    let runtime =
       unixcommand (fun s -> CCUnix.call "rt/pmars-0.9.4/pmars -A -@ rt/pmars-0.9.4/config/94b.opt %s" s) in
+    
     let name : string = "execute" in
     let oracle : oracle = Expected in
-    let runtime : runtime = Runtime command in
-    let action : action = Execute in
-    tests_from_dir ~name ~compiler ~oracle ~runtime ~action "bbctests" in
+    let action : action = IgnoreOutput in
+    tests_from_dir ~name ~compiler ~runtime ~oracle ~action "bbctests" in
   
   run "Tests corewars-compiler" (ocaml_tests @ bbc_tests @ verify_tests)
